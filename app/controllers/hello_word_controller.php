0561ec98774eda215c71de5fa972158501eb5623
Markus Mustonen <makesmi@hotmail.com>
2015-02-06 11:14:15 +0200

    näkymäsuunnitelmia

--- app/controllers/hello_word_controller.php
+++ app/controllers/hello_word_controller.php
@@ -3,11 +3,28 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-   	  self::render_view('home.html');
+        echo 'Tämä on etusivu!';
+    }
+    
+    public static function etusivu(){
+        self::render_view('suunnitelmat/etusivu.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä	
-      echo 'Hello World!';
+    public static function aihealue(){
+        self::render_view('suunnitelmat/aihealue.html');
     }
+    
+    public static function viestiketju(){
+        self::render_view('suunnitelmat/viestiketju.html');
+        
+    }
+    
+    public static function uusiKetju(){
+        self::render_view('suunnitelmat/uusiketju.html');
+    }
+    
+    public static function sandbox(){
+        self::render_view('helloworld.html');
+    }
+    
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,24 @@
   $app->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $app->get('/etusivu', function() {
+      HelloWorldController::etusivu();
+  });
+
+  $app->get('/aihealue', function() {
+      
+      HelloWorldController::aihealue();
+  });
+  
+  $app->get('/viestiketju', function() {
+      
+      HelloWorldController::viestiketju();
+  });
+  
+  $app->get('/uusiketju', function() {
+      
+      HelloWorldController::uusiKetju();
+  });
+  
+  
